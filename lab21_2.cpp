aacf7f61f574dac80e1374a517666cdb724cf54f
Nanashikung <nata.station@hotmail.com>
2022-02-01 17:42:41 +0700

    lab21_2.cpp complete

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+	double a,b,c,d;
+	a = R1->x + R1->w;
+	b = R2->x;
+	c = R2->y - R2->h;
+	d = R1->y;
+	if(a-b < 0 or d-c < 0) return 0;
 
-
-
+	double x,y;
+	if(a - b >= R2->w) x = R2->w;
+	else x = a -b;
+	if(d - c >= R2->h) y = R2->h;
+	else y = d-c;
+	return x*y;
 }
